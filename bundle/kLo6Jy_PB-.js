916e9a7c0216f22528ca276828f8483d086114a0
Vukory <Vukory@users.noreply.github.com>
2026-07-19 21:19:32 +0000

    deploy: 3324957011d04384baa7076db6d5a3cbccbca5fa

--- bundle/kLo6Jy_PB-.js
+++ bundle/kLo6Jy_PB-.js
@@ -26,11 +26,43 @@ class MultipleChoice extends HTMLElement {
   }
 
   customElements.define('vuko-multiple-choice', MultipleChoice);
-const { search } = window.location;
+const IGNORED_FIELDS = ['email', 'altcha'];
+
+  const { search } = window.location;
   const urlSearchParams = new URLSearchParams(search);
   const variant = urlSearchParams.get('utm_content');
+  const form =/** @type {HTMLFormElement} */ (document.getElementById('form'));
 
   if (variant) {
-    const form = /** @type {HTMLFormElement} */ (document.querySelector('form'));
     form.action += `?utm_content=${encodeURIComponent(variant)}`;
+  }
+
+  form.addEventListener('submit', (event) => {
+    const data = new FormData(form);
+    let isValid = false;
+
+    for (const [k, v] of data.entries()) {
+      if (IGNORED_FIELDS.includes(k)){
+        continue;
+      }
+
+      if (v !== null && v !== undefined && v !== '') {
+        isValid = true;
+      }
+    }
+
+    if (!isValid) {
+      event.preventDefault();
+      const submit = /** @type {HTMLButtonElement} */(document.getElementById('submit'));
+      submit.setCustomValidity('Please answer at least one of the questions.');
+      form.reportValidity();
+    }
+  });
+
+  const controls = form.elements;
+  
+  for (const control of controls) {
+    control.addEventListener('change', () => {
+      submit.setCustomValidity('');
+    });
   }
