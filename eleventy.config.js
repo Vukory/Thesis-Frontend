a442696111d9ad3a65cf8f8e36e7d9af7c710d91
Vukory <merjembajramovic8@gmail.com>
2026-07-20 23:15:29 +0200

    fix(dx): hot reload image overflow
    This fixes the problem with the illsutration overflow
    in the ALTCHA element on hot reload.

--- eleventy.config.js
+++ eleventy.config.js
@@ -49,6 +49,9 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/robots.txt');
   eleventyConfig.addWatchTarget('./src/**/*.css');
 
+  // Disabling due to quirk with altcha styles spilling out on reload.
+  eleventyConfig.setServerOptions({ domDiff: false });
+
   eleventyConfig.addPassthroughCopy('./src/assets/', {
     /**
      * 
